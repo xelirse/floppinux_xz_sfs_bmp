70ce6a011efa9da1a42f54b163c490cef69663d7
xelirse <passymas@gmail.com>
2026-04-16 06:43:34 +0000

    Add files via upload

--- ms_windows_101/w101_crear_pdf.sh
+++ ms_windows_101/w101_crear_pdf.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cupsfilter ./windows1fda.fasm.ndisasm.asm > ./windows1fda.fasm.ndisasm.asm.pdf
+cat ./windows1fda.fasm.ndisasm.asm | sed -E "s/\(.+//g" > ./windows1fda.fasm.ndisasm_v0.asm
+cupsfilter ./windows1fda.fasm.ndisasm_v0.asm > ./windows1fda.fasm.ndisasm_v0.asm.pdf
